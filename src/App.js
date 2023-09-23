4c1158c82be7f22dbe5d471c59604790e189ff4e
wookk456 <wookk456@dgist.ac.kr>
2023-09-23 14:56:27 +0900

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 5,
+      newCount : ""
+    }
+  }
+
+  isEven = (val) => {
+    return val % 2 ===0? "Even" : "Odd";
+  }
+
+  updateNewCount = (event) => {
+    this.setState({newCount: event.target.value})
+  }
+
+  updateCount = () => {
+    this.setState({count: Number(this.state.newCount),
+                  newCount: ""})
+  }
+
+  getClassName = (val) => {
+    return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1"
+  }
+
+  handleClick = () => this.setState({count: this.state.count + 1})
+
+  render = () =>
+    <div>
+      <table className="table table-striped table-bordered table-sm">
+        <thead className="table-primary text-white">
+          <tr><th>Value</th><th>Even?</th></tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{this.state.count}</td>
+            <td>{this.isEven(this.state.count)}</td>
+          </tr>
+        </tbody>
+        <tfoot className="text-center">
+          <tr>
+            <td colSpan="2">
+              <button className="btn btn-info m-2" onClick={this.handleClick}>
+                Click Me to Plue One
+              </button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+      {/* <h4 className={this.getClassName(this.state.count)}>
+        <button className="btn btn-info m-2" onClick = {this.handleClick}>
+         Click Me to Plus 1
+        </button>
+        Number of things: { this.isEven(this.state.count) }
+      </h4> */}
+
+      <div className="container-fluid">
+        Update Value
+        <div className="my-1">
+          <div className="col-lg-1">
+            <input className="form-control"
+             value = {this.state.newCount}
+             onChange={this.updateNewCount}/>
+          </div>
+          <button className="btn btn-info m-2"
+            onClick={ this.updateCount }>Update</button>
+        </div>
+
+
+      </div>
     </div>
-  );
-}
 
-export default App;
+
+}
